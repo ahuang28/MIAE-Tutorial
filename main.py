f682b6bf12d47936a9fe024feb4a72e68e4a532d
ahuang28 <amyhuang0605@gmail.com>
2022-01-29 11:53:15 -0500

    Another commit after merge.

--- main.py
+++ main.py
@@ -3,6 +3,7 @@ def main_wrapper():
     print(f"This is the start of our Python project. This function's name is {main_wrapper.__name__}")
 
     # Code here
+    utils.solid_example_1(example_param_1="a", example_param_2=1)
 
     print("This is the end of our Python project.")
 
